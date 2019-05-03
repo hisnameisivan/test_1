7ac823d34d4d55e9bfb98c315d06bd50f2f2bf3d
Walda rivers Addam <waddam@mi-w2.21-school.ru>
2019-05-03 22:51:20 +0300

    modify struct, some additions

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int		main()
+{
+	char	*s = "11231;6;Russia";
+	char	*t;
+
+	t = strrchr(s, ';');
+	t++;
+	// t = strchr(t, ';');
+	printf("%s\n", t);
+	printf("%d\n", atoi(t));
+	return (0);
+}
--- test.c
+++ test.c
@@ -6,19 +6,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFF_SIZE 5
+#define BUFF_SIZE 64
 
-typedef struct		s_list
+typedef struct			s_list
 {
-	char			*country;
-	size_t			sum;
-	int				count_uniq;
-	struct s_list	*next;
-}					t_list;
+	char				*country;
+	size_t				sum;
+	int					count_uniq;
+	struct s_id_list	*id_list;
+	struct s_list		*next;
+}						t_list;
 
-//TODO validation
+typedef struct			s_id_list
+{
+	int					id;
+	struct s_id_list	*next;
+}						t_id_list;
 
-char	*strnew(size_t size)
+//TODO validation (1. strlen по id (5 символов))
+
+char		*strnew(size_t size)
 {
 	char	*temp;
 	size_t	i;
@@ -34,7 +41,7 @@ char	*strnew(size_t size)
 	return (temp);
 }
 
-char	*strjoin(char const *s1, char const *s2)
+char		*strjoin(char const *s1, char const *s2)
 {
 	char	*temp;
 	size_t	i;
@@ -60,7 +67,7 @@ char	*strjoin(char const *s1, char const *s2)
 	return (temp);
 }
 
-char	*strsub(char const *s, unsigned int start, size_t len)
+char		*strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*temp;
 	size_t	i;
@@ -82,7 +89,18 @@ char	*strsub(char const *s, unsigned int start, size_t len)
 		return (NULL);
 }
 
-t_list	*create_node(char const *country, size_t sum, int count_uniq)
+t_id_list	*create_id_node(int id)
+{
+	t_id_list	*temp;
+
+	if (!(temp = (t_id_list *)malloc(sizeof(t_id_list))))
+		return (NULL);
+	temp->id = id;
+	temp->next = NULL;
+	return (temp);
+}
+
+t_list		*create_node(char const *country, size_t sum, int count_uniq)
 {
 	t_list	*temp;
 
@@ -93,6 +111,7 @@ t_list	*create_node(char const *country, size_t sum, int count_uniq)
 		temp->country = NULL;
 		temp->sum = 0;
 		temp->count_uniq = 0;
+		temp->id_list = NULL;
 		temp->next = NULL;
 	}
 	else
@@ -105,12 +124,26 @@ t_list	*create_node(char const *country, size_t sum, int count_uniq)
 		strcpy(temp->country, country);
 		temp->sum = sum;
 		temp->count_uniq = count_uniq;
+		temp->id_list = NULL;
 		temp->next = NULL;
 	}
 	return (temp);
 }
 
-void	add_node_back(t_list *begin, t_list *new)
+void		add_id_node_back(t_id_list *begin, t_id_list *new)
+{
+	t_id_list	*temp;
+
+	if (begin && new)
+	{
+		temp = begin;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new;
+	}
+}
+
+void		add_node_back(t_list *begin, t_list *new)
 {
 	t_list	*temp;
 
@@ -122,20 +155,26 @@ void	add_node_back(t_list *begin, t_list *new)
 		temp->next = new;
 	}
 }
-void	ft_lstaddb(t_list **alst, t_list *new)
-{
-	t_list	*temp;
 
-	if (alst && new)
+char		*read_file(int fd)
+{
+	int		ret;
+	char	buf[BUFF_SIZE + 1];
+	char	*temp;
+	char	*reading;
+
+	reading = NULL;
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		temp = *alst;
-		while (temp->next)
-			temp = temp->next;
-		temp->next = new;
+		buf[ret] = '\0';
+		temp = strjoin(reading, buf);
+		free(reading);
+		reading = temp;
 	}
+	return (reading);
 }
 
-char	*split_reading(char **reading)
+char		*split_reading(char **reading)
 {
 	char	*temp;
 	char	*one_string;
@@ -152,50 +191,97 @@ char	*split_reading(char **reading)
 	return (one_string);
 }
 
-int		analyze_string(char *str)
+void		fill_struct(t_list *list, char *str)
 {
-	return (strcmp(str, "user_id;count;country"));
+	t_id_list	*id_temp;
+	char		*temp;
+
+	temp = strrchr(str, ';');
+	list->country = ++temp;
+	temp = strchr(str, ';');
+	list->sum += atoi(++temp);
+	// if (list)
+	// {
+	// 	if (list->id_list == NULL)
+	// 	{
+	// 		list->id_list = create_id_node(atoi(one_string));
+	// 		id_temp = list->id_list;
+	// 	}
+	// 	else
+	// 		add_id_node_back(list->id_list, id_temp = create_id_node(atoi(one_string)));
+	// }
 }
+
+t_list		*find_node(t_list *begin, char *str)
+{
+	char	*temp_str;
+	t_list	*temp_node;
+
+	temp_node = begin;
+	temp_str = strrchr(str, ';');
+	temp_str++;
+	while (temp_node)
+	{
+		if (strcmp(temp_node->country, temp_str) == 0)
+			return (temp_node);
+		temp_node = temp_node->next;
+	}
+	return (NULL);
+}
+
+// int			analyze_string(char *str)
+// {
+// 	return (strcmp(str, "user_id;count;country"));
+// }
 
 int		main(int argc, char **argv)
 {
-	int		fd;
-	int		ret;
-	char	buf[BUFF_SIZE + 1];
-	char	*temp;
-	char	*reading;
-	char	*one_string;
-	t_list	*begin;
+	int			fd;
+	char		*reading;
+	char		*one_string;
+	t_list		*begin;
+	t_list		*temp;
 
-	temp = NULL;
-	reading = strnew(0);
-	one_string = NULL;
 	begin = NULL;
+	temp = NULL;
 	fd = open(argv[1], O_RDONLY);
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
-	{
-		//ret = read(fd, buf, BUFF_SIZE);
-		if (ret == -1)
-			return (-1); //TODO
-		buf[ret] = '\0';
-		temp = strjoin(reading, buf);
-		free(reading);
-		reading = temp;
-	}
+	if ((reading = read_file(fd)) == NULL)
+		return (-1); //TODO
 	// printf("1. reading = %s\n", reading);
-	one_string = split_reading(&reading);
+	// one_string = split_reading(&reading);
 	// printf("1. one_string = %s\n", one_string);
 	// printf("2. reading = %s\n", reading);
 	while (*reading != '\0')
 	{
-		if (analyze_string(one_string) == 0)
+	//	if (analyze_string(one_string) == 0)
 			one_string = split_reading(&reading);
 		// printf("2. one_string = %s\n", one_string);
 		if (begin == NULL)
+		{
 			begin = create_node(NULL, 0, 0);
+			temp = begin;
+		}
 		else
-			add_node_back(begin, create_node(NULL, 0, 0));
-
+		{
+			if ((temp = find_node(begin, one_string)) == NULL)
+				add_node_back(begin, temp = create_node("NULL", 0, 0));
+		}
+		// if (find_node(begin) == NULL)
+		// 	add_node_back(begin, temp = create_node("NULL", 0, 0));
+		// else
+		// 	temp = find_node(begin);
+		fill_struct(temp, one_string);
+	}
+	while (begin)
+	{
+		// id_temp = temp->id_list;
+		// while (id_temp)
+		// {
+		// 	printf("%d\n", id_temp->id);
+		// 	id_temp = id_temp->next;
+		// }
+		printf("%s , %zu, %d\n", begin->country, begin->sum, begin->count_uniq);
+		begin = begin->next;
 	}
 	// printf("3. reading = %s\n", reading);
 	// printf("3. one_string = %s\n", one_string);
